6057af9aa3119d24cbade568f2ab2565221fc32f
Fernanda Dorea <dorea.meyer@gmail.com>
2016-01-12 10:20:58 +0100

    Update files to latest version sent to Tariq

--- Basic.R
+++ Basic.R
@@ -1,5 +1,5 @@
 source("sourceASF.R")
 sourceASF()
 indexHerdFile<-read.table("IndexHerds.csv",sep=";")
-optC<-ASFoptions(maxTime=365,n=1000,indexHerdSelect=list(ID=indexHerdFile[[1]]),runID="BasicAO",seed=-10)
+optC<-ASFoptions(maxTime=365,n=1000,indexHerdSelect=list(ID=indexHerdFile[[1]]),runID="Basic",seed=-10)
 a<-ASF(optC)
